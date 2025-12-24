2528be172420228cef84f88400586a442fc952e3
bala-amypo <bala@amypo.in>
2025-12-24 05:07:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AppUserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AppUserServiceImpl.java
@@ -1,29 +1,26 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.AppUser;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.AppUserRepository;
 import com.example.demo.service.AppUserService;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service   // ✅ REQUIRED
 public class AppUserServiceImpl implements AppUserService {
 
-    private final AppUserRepository repository;
+    private final AppUserRepository appUserRepository;
 
-    public AppUserServiceImpl(AppUserRepository repository) {
-        this.repository = repository;
+    public AppUserServiceImpl(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
     }
 
     @Override
     public AppUser getUserByEmail(String email) {
-        return repository.findByEmail(email)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("User not found"));
+        return appUserRepository.findByEmail(email).orElse(null);
     }
 
     @Override
     public AppUser createUser(AppUser user) {
-        return repository.save(user);
+        return appUserRepository.save(user);
     }
 }
